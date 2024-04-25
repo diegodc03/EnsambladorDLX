9ac87718ff476ab959f45c8136eed5819d12f38c
Diego de Castro Merillas <116064701+diegodc03@users.noreply.github.com>
2024-04-25 10:14:26 +0200

    Falta añadir los valores .float correctamente
    seria con lf y sf, ya que es load float

--- ensamblador.s
+++ ensamblador.s
@@ -3,7 +3,6 @@
 ;Carlos Conde VIcente
 ;Diego de Castro Merillas
 
-
 ; 	A[0] = valor_inicial
 ; 	A[n] = Si A[n-1] 	es par	A[n] = A[n-1] / 2
 ;						es impar  A[n-1] = 3*A[n-1] + 1
@@ -11,16 +10,14 @@
 
 
 ;	Declaración de directivas del ordenador
-
-.data
+		.data
 
 PrintFormat:	.asciiz		"%d\n"
-		.align		2
+				.align		2
 PrintPar:	.word		PrintFormat
+PrintValue:	.space		4
 
-	
-
-valor_inicial: .word 97
+valor_inicial: .word 10
 
 ;; VARIABLES DE SALIDA:
 secuencia: .space 120*4
@@ -29,7 +26,6 @@ secuencia_maximo: .word 0
 secuencia_valor_medio: .float 0
 lista: .space 9*4
 lista_valor_medio: .float 0
-
 ;; FIN VARIABLES DE ENTRADA Y SALIDA
 
 
@@ -37,10 +33,12 @@ lista_valor_medio: .float 0
 		.global main
 
 main:
-	addi	r2, r0, valor_inicial
-	add	r3, r2, r0
-	add	r4, r2, r0
-	addi	r5, r0, 3
+	lw	r2, valor_inicial ; N
+	add		r3, r2, r0 ; A[n] 
+	add		r4, r2, r0 ; A[n-1]
+	addi	r5, r0, 3 ; 3
+	addi	r16, r0, 0 ; valor medio
+	add		r17, r0, r2 ; secuencia_maximo
 
 
 
@@ -48,65 +46,71 @@ loop:
 
 	; Añadimos estadisticas
 	
-	;añadimos secuencia_tamaño
-	add		r6, r0, secuencia_tamanho
-	addi	r6, r6, 1
-	add		secuencia_tamanho, r0, r6
+	
+	;Sumamos el valor medio
+	lw		r16, secuencia_valor_medio
+	add		r16, r16, r3 
+	sw		secuencia_tamanho, r6
+	sw		secuencia_valor_medio, r16
 
-
+	
 	;Añadimos al array secuencia un valor nuevo
 
 	;Añadimos a r8 el valor que queremos añadir
-	addi	r8, r0, r3
+	add		r8, r4, r0
 	;Añadimos a r11 4 para los bytes y la secuencia
 	addi	r11, r0, 4
-	;carga registro base en r9
-	la		r9, secuencia
 	;multiplicamos para saber justo la direccion
-	mult	r10, secuencia_tamanho, r11
-	;movemos la direccion
-	add		r9, r9, r10
-	sw		r8, 0(r9)  
+	mult	r10, r6, r11
+	;carga registro base en r9
+	add		r9, r0, secuencia
 	
+	add		r9, r9, r10
+	;movemos la direccion
+	sw		0(r9), r8   
+	
+	
+	
+	;añadimos secuencia_tamaño
+	lw		r6, secuencia_tamanho	
+	addi	r6, r6, 1
 
-
-	;Añadimos el valor máximo	r12 es 1 si r3 > secuencia_maximo
-	sgt		r12, r3, secuencia_maximo
-	beqz	no_mayor
-	addi	secuencia_maximo, r0, r3
+	;Añadimos el valor máximo	r12 es 1 si r3 > secuencia_maximo	
+	;Bajamos a Registros el valor en memoria
+	lw		r17, secuencia_maximo
+	;Comprobamos valor
+	sgt		r7, r3, r17
+	beqz	r7, no_mayor
+	add		r17, r0, r3
+	; Añadimos nuevo valor a la variable
+	sw		secuencia_maximo, r17
 	no_mayor:
 	
 
-	;Añadimos el valor mínimo
-	slt		r12, r3, secuencia_minimo
-	beqz	no_menor
-	addi	secuencia_minimo, r0, r3
-	no_menor:
 
-
-
-	subi	r6, r3, 1	; Si A[n-1] es 1 finaliza
+	subi	r7, r3, 1	; Si A[n-1] es 1 finaliza
 	jal	print
-	beqz	r6, finish
+	beqz	r7, finish
 
-	andi	r7, r3, 1	; ; Si A[n-1] es par
-	beqz	r7, par
+	andi	r12, r3, 1	; ; Si A[n-1] es par
+	beqz	r12, par
 	mult	r4, r3, r5	; Si A[n-1] es impar
 	addi	r4, r4, 1 
 	add		r3, r4, r0	; A[n-1] = A[n]
 	
-	
+
 	j	loop
 
 
 
 par:
 	srli	r4, r3, 1	; A[n] = A[n-1]/2
-	add	r3, r4, r0	; A[n-1] = A[n]
+	add		r3, r4, r0	; A[n-1] = A[n]
 	j	loop
 
 
 
+; Para que se escriban los valores que se van calculando
 print:
 	sw	PrintValue, r4
 	addi	r14, r0, PrintPar
@@ -114,6 +118,99 @@ print:
 	jr	r31
 
 
-finish:
+; comprobacion de los valores del array lista
+print1:
+	add	r20, r0, lista
+	addi	r20, r20, 12
+	lw		r21, 0(r20)
+	sw	PrintValue, r21
+	addi	r14, r0, PrintPar
+	trap	5
+	jr	r31
 
+
+; Esto sirve para la comprobacion de los valores del array secuencia
+print2:
+	add	r20, r0, secuencia
+	;addi	r20, r20,4
+	lw		r21, 0(r20)
+	sw		PrintValue, r21
+	addi	r14, r0, PrintPar
+	trap	5
+	jr	r31
+
+
+
+finish:
+	jal		estadisticasFinales
 	trap	0
+
+
+
+estadisticasFinales:
+	;Tenemos que rellenar lista_valor_medio y lista con los valores.
+	; Luego habrá que mostrarlos
+	;vT=secuencia_tamanho, vIni=valor_inicial, vMax=secuencia_maximo y vMed=secuencia_valor_medio
+	lw		r6, valor_inicial
+	lw		r7, secuencia_tamanho
+	lw		r8, secuencia_maximo
+	;Es la suma solo
+	lw		r9, secuencia_valor_medio
+	;Tenemos secuencia_valor_medio
+	div		r9, r9, r7
+
+
+	; vamos sumandole 4 a lista y añadiendole elementos
+	add		r20, r0, lista
+	
+	
+	; vIni*vT
+	mult	r10, r6, r7
+	sw		0(r20), r10
+	addi	r20, r20, 4
+	; vMax*vT
+	mult	r11, r8, r7
+	sw		0(r20), r11
+	addi	r20, r20, 4
+	; vMed*vT
+	mult	r12, r9, r7
+	sw		0(r20), r12
+	addi	r20, r20, 4
+	; (vIni/vMax)*vT
+	div		r13, r6, r8
+	mult	r13, r13, r7
+	sw		0(r20), r13
+	addi	r20, r20, 4
+	; (vIni/vMed)*vT
+	div		r14, r6, r9
+	mult	r14, r14, r7
+	sw		0(r20), r14
+	addi	r20, r20, 4
+	; (vMax/vIni)*vT
+	div 	r14, r8, r6
+	mult	r14, r14, r7
+	sw		0(r20), r15
+	addi	r20, r20, 4
+	; (vMax/vMed)*vT
+	div		r15, r8, r9
+	mult	r15, r15, r7
+	sw		0(r20), r16
+	addi	r20, r20, 4
+	; (vMed/vIni)*vT
+	div		r16, r9, r6
+	mult	r16, r16, r7
+	sw		0(r20), r17
+	addi	r20, r20, 4
+	; (vMed/vMax)*vT
+	div		r17, r9, r8
+	mult	r17, r17, r7
+	sw		0(r20), r18
+	addi	r20, r20, 4
+
+
+	;Ahora tenemos todos los registros añadidos con las variables, tenemos que introducir
+	;jal print1
+
+	jalr r31 
+
+	
